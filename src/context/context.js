ef5def8a045310861bd9a0520e9de5a139da1583
Arabin1 <mdasaduzzamanarabin@gmail.com>
2023-07-08 17:05:14 +0600

    Updated context.js file

--- src/context/context.js
+++ src/context/context.js
@@ -19,20 +19,15 @@ export const ThemeProvider = (props) => {
   const themeLocalStorageKey = "theme";
 
   const initialState = () => {
-    let storedState = "";
-    if (typeof window !== "undefined") {
-      storedState = window.localStorage.getItem(themeLocalStorageKey);
-    }
+    const storedState = window.localStorage.getItem(themeLocalStorageKey);
     return storedState ? JSON.parse(storedState) : { lightTheme: true };
   };
 
   const [state, dispatch] = useReducer(themeReducer, null, initialState);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.localStorage.setItem(themeLocalStorageKey, JSON.stringify(state));
-    }
-  }, []);
+    window.localStorage.setItem(themeLocalStorageKey, JSON.stringify(state));
+  }, [state]);
 
   return (
     <themeContext.Provider value={{ state, dispatch }}>
